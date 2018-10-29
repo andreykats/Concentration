f397e578f23fb69629984995eb5b3d4f08874b57
Andrey Kats <andrey@gramercytech.com>
2018-10-29 09:25:24 -0400

    First commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Andrey on 10/29/18.
+//  Copyright © 2018 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
